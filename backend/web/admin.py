62f49ea06ca90250a3fe8af70ff86fa43225c42f
bai kk <1335254659@qq.com>
2026-03-11 23:01:20 +0800

    数据库更新

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,3 +1,6 @@
 from django.contrib import admin
+from web.models.user import UserProfile
 
-# Register your models here.
+@admin.register(UserProfile)
+class UserProfileAdmin(admin.ModelAdmin):
+    raw_id_fields = ("user",) #逗号不要删，这里传一个列表，括号表示一个元素 加逗号变为列表
