2c57dea2eadfe5b06d3b5d86a8fa2aead9a612d5
bai kk <1335254659@qq.com>
2026-03-14 19:37:47 +0800

    实现创作页面的增删改查

--- backend/web/admin.py
+++ backend/web/admin.py
@@ -1,6 +1,13 @@
 from django.contrib import admin
 from web.models.user import UserProfile
+from web.models.character import Character
+
 
 @admin.register(UserProfile)
 class UserProfileAdmin(admin.ModelAdmin):
     raw_id_fields = ("user",) #逗号不要删，这里传一个列表，括号表示一个元素 加逗号变为列表
+
+
+@admin.register(Character)
+class CharacterAdmin(admin.ModelAdmin):
+    raw_id_fields = ("author",)
